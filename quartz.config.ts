56f0c372b13e361c3c308251101360e22e2d22b6
Herlon Targino <herlon@duck.com>
2026-04-23 21:32:26 -0300

    disable SPA to fix routing

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "ACON-B 2026",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
